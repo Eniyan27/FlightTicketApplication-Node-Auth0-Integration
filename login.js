c1547c62178165a86322c61c704b3d84ecb3cb85
Eniyan Maran <74698190+Eniyan27@users.noreply.github.com>
2022-04-25 23:32:05 +0530

    Added Auth0

--- login.js
+++ login.js
@@ -1,7 +1,17 @@
-// const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+	authRequired: false,
+	auth0Logout: true,
+	secret: process.env.SECRET,
+	baseURL: process.env.BASEURL,
+	clientID: process.env.CLIENTID,
+	issuerBaseURL: process.env.ISSUER
+  };
 
 const app = express();
 
@@ -14,6 +24,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.static(path.join(__dirname, 'images')))
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+	res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
 
 app.get('/', function(request, response) {
 
@@ -23,6 +38,7 @@ app.get('/', function(request, response) {
 app.post('/home', function(request, response) {
 	response.sendFile(path.join(__dirname + '/home.html'));
 });
+
 
 app.listen(3000);
 
